c4200c0dc3e72e491debf8603b530a4ea1c51f8d
LordLoras <moonknight999@gmail.com>
2020-06-14 00:38:17 +0300

    Add project files.

---
+++ DrawingAppWPF/Draw.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows;
+
+namespace DrawingAppWPF
+{
+    
+    class Draw
+    {
+        Random rand = new Random();
+
+        #region DrawRectangle
+        public void DrawRectangle(Canvas drawPanel, string fillColor, string strokeThickness, string height, string width, double opacity)
+        {
+            Rectangle rect = new Rectangle();
+            Brush brush = new SolidColorBrush();
+            Color color;
+            try
+            {
+                //try to parse user input into proper color otherwise throw exception   
+                color = (Color)ColorConverter.ConvertFromString(fillColor);
+                brush = new SolidColorBrush(color);
+            }
+            catch (Exception)
+            {
+                System.Windows.MessageBox.Show("Invalid Color! Please input new color");
+            }
+            //Set width and heigth of the rect and the border thickness
+            try
+            {
+                rect.StrokeThickness = int.Parse(strokeThickness);
+                rect.Height = int.Parse(height);
+                rect.Width = int.Parse(width);
+            }
+            catch (Exception) { System.Windows.MessageBox.Show("Please enter valid numbers for heighth/width and stroke thickness"); }
+
+            //set border as black color
+            rect.Stroke = Brushes.Black;
+            //fill the shape with color from string
+            rect.Fill = brush;
+            //set the opacity
+            rect.Opacity = opacity;
+            //add the rect to the canvas           
+            drawPanel.Children.Add(rect);
+            //put the shape in random position after adding it
+            RandomPosition(rect);
+        }
+        #endregion
+
+        #region DrawEllipse
+        public void DrawEllipse(Canvas drawPanel, string fillColor, string strokeThickness, string height, string width, double opacity)
+        {
+            Ellipse ellipse = new Ellipse();
+            Brush brush = new SolidColorBrush();
+            Color color;
+            try
+            {
+                //try to parse user input into proper color otherwise throw exception   
+                color = (Color)ColorConverter.ConvertFromString(fillColor);
+                brush = new SolidColorBrush(color);
+            }
+            catch (Exception)
+            {
+                System.Windows.MessageBox.Show("Invalid Color! Please input new color");
+            }
+            //Set width and heigth of the ellipse and the border thickness
+            try
+            {
+                ellipse.StrokeThickness = int.Parse(strokeThickness);
+                ellipse.Height = int.Parse(height);
+                ellipse.Width = int.Parse(width);
+            }
+            catch (Exception) { System.Windows.MessageBox.Show("Please enter valid numbers for heighth/width and stroke thickness"); }
+
+            //set border as black color
+            ellipse.Stroke = Brushes.Black;
+            //fill the shape with color from string
+            ellipse.Fill = brush;
+            //set the opacity
+            ellipse.Opacity = opacity;
+            //add the rect to the canvas           
+            drawPanel.Children.Add(ellipse);
+            //put the shape in random position after adding it
+            RandomPosition(ellipse);
+        }
+        #endregion
+
+        #region DrawLine
+        public void DrawLine(Canvas drawPanel, string fillColor, string strokeThickness, double opacity)
+        {
+            Line line = new Line();
+            Brush brush = new SolidColorBrush();
+            Color color;
+            try
+            {
+                //try to parse user input into proper color otherwise throw exception   
+                color = (Color)ColorConverter.ConvertFromString(fillColor);
+                brush = new SolidColorBrush(color);
+            }
+            catch (Exception)
+            {
+               MessageBox.Show("Invalid Color! Please input new color");
+            }
+            //Set line thickness
+            try
+            {
+                line.StrokeThickness = int.Parse(strokeThickness);
+            }
+            catch (Exception) { MessageBox.Show("Please enter valid numbers for heighth/width and stroke thickness"); }
+
+            //set border as black color
+            line.Stroke = brush;
+            //set the opacity
+            line.Opacity = opacity;
+            line.X1 = 1;
+            line.X2 = rand.Next(1, 500);
+            line.Y1 = 1;
+            line.Y2 = rand.Next(20, 70);
+            //add the rect to the canvas           
+            drawPanel.Children.Add(line);
+            //put the shape in random position after adding it
+            RandomPosition(line);
+        }
+        #endregion
+
+        #region DrawTriangle
+        public void DrawTriangle(Canvas drawPanel, string fillColor, string strokeThickness, double opacity)
+        {
+            Brush brush = new SolidColorBrush();
+            Color color;
+            try
+            {
+                //try to parse user input into proper color otherwise throw exception   
+                color = (Color)ColorConverter.ConvertFromString(fillColor);
+                brush = new SolidColorBrush(color);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Invalid Color! Please input new color");
+            }
+            Polygon triangle = new Polygon();
+            triangle.Stroke = Brushes.Black;
+            triangle.Fill = brush;
+            triangle.StrokeThickness = 2;
+            //make 3 points of a triangle
+            Point Point1 = new Point(0,90);
+            Point Point2 = new Point(0,0);
+            Point Point3 = new Point(90,0);
+            PointCollection trianglePoints = new PointCollection();
+            //add points to a collection and set them to the triangle
+            trianglePoints.Add(Point1);
+            trianglePoints.Add(Point2);
+            trianglePoints.Add(Point3);
+            triangle.Points = trianglePoints;
+            //set opacity
+            triangle.Opacity = opacity;
+            //draw triangle and set random location
+            drawPanel.Children.Add(triangle);
+            RandomPosition(triangle);        
+
+
+        }
+        #endregion
+
+        public void RandomPosition(Shape shape)
+        {
+            //Use GUID to generate random seeds otherwise .next() doesn't work properly. Bug?
+            Canvas.SetTop(shape, new Random(Guid.NewGuid().GetHashCode()).Next(75, 500));
+            Canvas.SetLeft(shape, new Random(Guid.NewGuid().GetHashCode()).Next(0, 1180));
+        }
+
+        public void DrawSolution(Canvas drawPanel)
+        {
+            Polygon shape = new Polygon();
+            Polygon shape2 = new Polygon();
+            Polygon shape3 = new Polygon();
+            shape.Stroke = System.Windows.Media.Brushes.Black;
+            shape.Fill = System.Windows.Media.Brushes.LightSeaGreen;
+            shape.StrokeThickness = 2;
+            PointCollection myPointCollection = new PointCollection();
+            Point Point1 = new Point(0, 0);
+            Point Point2 = new Point(50, 0);
+            Point Point3 = new Point(50, 40);
+            Point Point4 = new Point(0, 40);
+            myPointCollection.Add(Point1);
+            myPointCollection.Add(Point2);
+            myPointCollection.Add(Point3);
+            myPointCollection.Add(Point4);
+            shape.Points = myPointCollection;
+            drawPanel.Children.Add(shape);
+
+            Point1 = new Point(50, 0);
+            Point2 = new Point(100, 0);
+            Point3 = new Point(100, 40);
+            Point4 = new Point(50, 40);
+            myPointCollection.Add(Point1);
+            myPointCollection.Add(Point2);
+            myPointCollection.Add(Point3);
+            myPointCollection.Add(Point4);
+            shape.Points = myPointCollection;
+            drawPanel.Children.Add(shape2);
+
+            Point1 = new Point(0, 40);
+            Point2 = new Point(100, 40);
+            Point3 = new Point(100, 80);
+            Point4 = new Point(0, 80);
+
+            
+            myPointCollection.Add(Point1);
+            myPointCollection.Add(Point2);
+            myPointCollection.Add(Point3);
+            myPointCollection.Add(Point4);
+            shape.Points = myPointCollection;
+            drawPanel.Children.Add(shape3);
+
+
+
+
+        }
+    
+    }
+}
---
+++ DrawingAppWPF/MainWindow.xaml.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace DrawingAppWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Draw draw = new Draw();
+        //LMB Down
+        bool mouseDown = false;
+        //UIelement under mouse
+        UIElement currElement;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+          
+  
+            
+        }
+
+        #region DrawButton_Click
+        private void DrawButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Call the corresponding method based on the combo box
+            switch(shapeBox.Text)
+            {
+                case "Rectangle":
+                draw.DrawRectangle(drawPanel, colorBox.Text, strokeThickness.Text, heightBox.Text, widthBox.Text, opacitySlider.Value);
+                    break;
+
+                case "Ellipse":
+                draw.DrawEllipse(drawPanel, colorBox.Text, strokeThickness.Text, heightBox.Text, widthBox.Text, opacitySlider.Value);
+                    break;
+                case "Line":
+                    draw.DrawLine(drawPanel, colorBox.Text, strokeThickness.Text, opacitySlider.Value);
+                    break;
+
+                case "Triangle":
+                    draw.DrawTriangle(drawPanel,colorBox.Text,strokeThickness.Text,opacitySlider.Value);
+                    break;
+            }
+
+
+        }
+        #endregion
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            //update the label for visualizing % opacity
+            opacityLabel.Content = Math.Round(opacitySlider.Value, 2).ToString();
+           
+        }
+        //read the xaml from filestream
+        public static Canvas LoadCanvas()
+        {
+            using (System.IO.FileStream fs = System.IO.File.Open("save.xaml", System.IO.FileMode.Open, System.IO.FileAccess.Read))
+            {
+                return System.Windows.Markup.XamlReader.Load(fs) as Canvas;
+            }
+        }
+        private void LoadBTN_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //check if the save file exist
+                if(System.IO.File.Exists("save.xaml"))
+                {
+                    //clear any existing shapes
+                    drawPanel.Children.Clear();
+                    
+                    //load shapes from file
+                    drawPanel.Children.Add(LoadCanvas());
+                    Canvas canvas = LoadCanvas();
+                   
+                }
+                else
+                {
+                    MessageBox.Show("Save file doesn't exist. Save before loading!");
+                }
+
+            }
+            catch(Exception ex) { MessageBox.Show(ex.ToString()); }
+        }
+
+        private void SaveBTN_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //store the canvas and its children markup in string and write it to file.
+                string xaml = System.Windows.Markup.XamlWriter.Save(drawPanel);
+                System.IO.FileStream fs = System.IO.File.Create("save.xaml");
+                System.IO.StreamWriter sw = new System.IO.StreamWriter(fs);
+                sw.Write(xaml);            
+                sw.Close();
+                fs.Close();
+            }
+            catch (Exception ex) { MessageBox.Show(ex.ToString()); }
+        }
+
+        #region drawPanel LMBDown
+        private void DrawPanel_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            //set mouse down state to true
+            mouseDown = true;
+            //get the UI element thats over the cursor and stick the mouse to the element.
+            currElement = Mouse.DirectlyOver as UIElement;
+            currElement.CaptureMouse();
+            
+        }
+        #endregion
+
+
+        #region drawPanel LMBUP
+        private void DrawPanel_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            //set mouse down state to false
+            mouseDown = false;
+            //release the mouse after LMB is let go
+            currElement.ReleaseMouseCapture();
+
+            //Delete operation on LMB UP
+            if (operationBox.SelectedIndex == 3)
+            {
+                drawPanel.Children.Remove(currElement);
+            }
+        }
+        #endregion
+
+        #region drawPanel MouseMove
+        private void DrawPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            //if the user is holding LMB down
+            if(mouseDown)
+            {
+                //get the mouse position and set the element position based on movement
+                Point mousePosition = e.GetPosition(drawPanel);
+
+                switch(operationBox.Text)
+                {
+                    case "Move":
+                        Canvas.SetTop(currElement, mousePosition.Y);
+                        Canvas.SetLeft(currElement, mousePosition.X);
+                        break;
+                    //Rotating is not working as intended due to needing more advanced math on polygons and different shape sizes
+                    case "Rotate":
+                        currElement.RenderTransform = new RotateTransform(mousePosition.X);
+                        break;
+                    //TO-DO
+                    case "Scale":
+                        break;
+                  
+                        
+                }
+                //test
+
+                testLBL.Content = "X: " + mousePosition.X.ToString() + " " + "Y: " + mousePosition.Y.ToString();
+                
+            }
+        }
+        #endregion
+
+        #region KeyUp KeyHook
+        private void MainWindow_KeyUp(object sender, KeyEventArgs e)
+        {
+            //draw a rectangle with key press F1
+            if(e.Key == Key.F1)
+            {
+                draw.DrawRectangle(drawPanel, colorBox.Text, strokeThickness.Text, heightBox.Text, widthBox.Text, opacitySlider.Value);
+            }
+            //Select operation to delete with F2
+            if(e.Key == Key.F2)
+            {
+                operationBox.SelectedIndex = 3;
+            }
+            //select operation to move with F3
+            if(e.Key == Key.F3)
+            {
+                operationBox.SelectedIndex = 1;
+            }
+        }
+        #endregion
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            draw.DrawSolution(drawPanel);
+        }
+    }
+}
